59d4567aaeb94d8c96eb59d77d6313533d55cad6
KarenPSMoreira <karenpdsmoreira@gmail.com>
2023-07-03 21:09:04 -0300

    Build: Implementing home and courses components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './courses/catalog/catalog.component';
+import { CourseDetailsComponent } from './courses/course-details/course-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'catalog',
+    component: CatalogComponent
+  },
+  {
+    path: 'course-details',
+    component: CourseDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CourseDetailsComponent } from './courses/course-details/course-details.component';
+import { CatalogComponent } from './courses/catalog/catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CourseDetailsComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
