5d07df9ff6d5e6e82f0367382ff06af0862f37f7
Guilherme Henrique <a226881906@fumec.edu.br>
2024-06-09 15:49:18 -0300

    Rebuild: Removing Duplicated Header Just Logo Component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,7 +6,6 @@ import { HeaderWithNavbarComponent } from './header-with-navbar/header-with-navb
 import { HeaderJustLogoComponent } from './header-just-logo/header-just-logo.component';
 import { MenuButtonsComponent } from './menu-buttons/menu-buttons.component';
 import { LoginAndSignUpCardComponent } from './login-and-sign-up-card/login-and-sign-up-card.component';
-import { HeaderJustLogoWhiteComponent } from './header-just-logo-white/header-just-logo-white.component';
 
 @NgModule({
   declarations: [],
@@ -18,7 +17,6 @@ import { HeaderJustLogoWhiteComponent } from './header-just-logo-white/header-ju
     HeaderJustLogoComponent,
     LoginAndSignUpCardComponent,
     MenuButtonsComponent,
-    HeaderJustLogoWhiteComponent
   ],
   exports: [
     HeaderComponent,
@@ -27,7 +25,6 @@ import { HeaderJustLogoWhiteComponent } from './header-just-logo-white/header-ju
     HeaderJustLogoComponent,
     LoginAndSignUpCardComponent,
     MenuButtonsComponent,
-    HeaderJustLogoWhiteComponent
   ]
 })
 export class ComponentsModule { }
--- src/app/components/header-just-logo/header-just-logo.component.ts
+++ src/app/components/header-just-logo/header-just-logo.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header-just-logo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header-just-logo.component.html',
   styleUrl: './header-just-logo.component.scss'
 })
 export class HeaderJustLogoComponent {
+  @Input() type: any = {
+    colorful: false,
+    white: false,
+  }
+  
+  constructor() { }
 
 }
