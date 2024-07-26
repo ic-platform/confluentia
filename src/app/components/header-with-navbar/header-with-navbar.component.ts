de58c709d7b9575f95452a94298521c47e0dda7f
Guilherme Henrique <a226881906@fumec.edu.br>
2024-07-26 15:33:32 +0000

    Build: Header With Navbar Component

--- src/app/components/header-with-navbar/header-with-navbar.component.ts
+++ src/app/components/header-with-navbar/header-with-navbar.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-with-navbar',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, RouterLink ],
   templateUrl: './header-with-navbar.component.html',
   styleUrl: './header-with-navbar.component.scss'
 })
 export class HeaderWithNavbarComponent {
+  constructor() {}
+
+  @Input() type: any = {
+    colorful: false,
+    white: false,
+  }
+
+  @Input() param: any = {};
+
+  
 
 }
