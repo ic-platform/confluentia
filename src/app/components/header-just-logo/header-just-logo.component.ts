a3ad8567479ae427850876c76ecef043400c6348
Guilherme Henrique <a226881906@fumec.edu.br>
2024-07-29 14:04:19 +0000

    Rebuild 5

--- src/app/components/header-just-logo/header-just-logo.component.ts
+++ src/app/components/header-just-logo/header-just-logo.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-just-logo',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header-just-logo.component.html',
   styleUrl: './header-just-logo.component.scss'
 })
--- src/app/public/certificate/certificate.component.ts
+++ src/app/public/certificate/certificate.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { HeaderWithNavbarComponent } from '../../components/header-with-navbar/header-with-navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificate',
   standalone: true,
-  imports: [HeaderComponent, HeaderWithNavbarComponent],
+  imports: [HeaderComponent, HeaderWithNavbarComponent, CommonModule],
   templateUrl: './certificate.component.html',
   styleUrl: './certificate.component.scss'
 })
 export class CertificateComponent {
+  isUserLogged() {
+    return false;
+  }
+
   paramLogged = {
     buttonText: 'Menu',
     router: '/main'
