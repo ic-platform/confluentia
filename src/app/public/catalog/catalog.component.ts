e3d0f8631ef370584b60c4acb58d7ea733e9d0d9
belaparreiras <isabelamparreiras@gmail.com>
2024-07-30 13:01:34 -0300

    Catalogo commit inicial

--- src/app/public/catalog/catalog.component.ts
+++ src/app/public/catalog/catalog.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { ComponentsModule } from '../../components/components.module';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ComponentsModule],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss'
 })
 export class CatalogComponent {
-
+  type: any = {
+    colorful: true,
+    white: false,
+  }
 }
