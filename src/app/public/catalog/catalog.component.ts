8a24b9761c8675e3750871852f0bf3764b10d441
Guilherme Henrique <a226881906@fumec.edu.br>
2024-07-30 19:54:25 +0000

    Rebuild 8

--- src/app/public/catalog/catalog.component.ts
+++ src/app/public/catalog/catalog.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { HeaderJustLogoComponent } from '../../components/header-just-logo/header-just-logo.component';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [HeaderJustLogoComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss'
 })
 export class CatalogComponent {
-
+  type: any = {
+    colorful: true,
+    white: false,
+  };
 }
