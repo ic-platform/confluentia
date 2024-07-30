c7716a836e985f61d6cc0c8f938612f1a7182d93
Guilherme Henrique <a226881906@fumec.edu.br>
2024-07-30 02:11:24 -0300

    Rebuild 7

--- src/app/private/profile/profile.component.ts
+++ src/app/private/profile/profile.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  constructor() {}
 
+  param = {
+    buttonText: 'Menu',
+    router: '/main'
+  };
 }
