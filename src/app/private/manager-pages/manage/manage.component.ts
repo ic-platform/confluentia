1ccf8d4976923eac3f25c306bc04d1e5394da5bf
belaparreiras <isabelamparreiras@gmail.com>
2024-07-30 15:13:09 -0300

    manage inicial

--- src/app/private/manager-pages/manage/manage.component.ts
+++ src/app/private/manager-pages/manage/manage.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { ComponentsModule } from '../../../components/components.module';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-manage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ComponentsModule],
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.scss'
 })
 export class ManageComponent {
-
+  type: any = {
+    colorful: true,
+    white: false,
+  }
 }
