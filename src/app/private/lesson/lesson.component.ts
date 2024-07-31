a72b5bfceb4517f9677fa03e1423eaaf63eedd48
Guilherme Henrique <a226881906@fumec.edu.br>
2024-07-31 11:11:55 -0300

    Rebuild 14

--- src/app/private/lesson/lesson.component.ts
+++ src/app/private/lesson/lesson.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
@@ -7,10 +8,9 @@ import { HeaderComponent } from '../../components/header/header.component';
   standalone: true,
   imports: [CommonModule, HeaderComponent],
   templateUrl: './lesson.component.html',
-  styleUrl: './lesson.component.scss'
+  styleUrls: ['./lesson.component.scss']
 })
 export class LessonComponent {
-
   param = {
     buttonText: 'Menu',
     router: '/main'
@@ -29,7 +29,9 @@ export class LessonComponent {
             'Navegação pela interface',
             'Barras de ferramentas e guias',
             'Personalização da faixa de opções'
-          ]
+          ],
+          videoLink: 'https://youtu.be/FPfQMVf4vwQ?si=sj2CgSlph54RZw1i',
+          resourceLink: 'https://example.com/resource1.pdf'
         },
         {
           title: 'Aula 2: Manipulação de Dados',
@@ -37,7 +39,9 @@ export class LessonComponent {
             'Inserção, exclusão e edição de células',
             'Formatação de células',
             'Gerenciamento de linhas e colunas'
-          ]
+          ],
+          videoLink: 'https://www.example.com/video2.mp4',
+          resourceLink: 'https://example.com/resource2.pdf'
         }
       ]
     },
@@ -50,27 +54,77 @@ export class LessonComponent {
             'Sintaxe básica das fórmulas',
             'Operadores matemáticos e lógicos',
             'Referências de células'
-          ]
+          ],
+          videoLink: 'https://www.example.com/video3.mp4',
+          resourceLink: 'https://example.com/resource3.pdf'
         }
       ]
     }
   ];
 
-  currentLesson: Lesson = this.modules[0].lessons[0];
+  currentLesson: Lesson;
+  currentModuleIndex = 0;
+  currentLessonIndex = 0;
+  safeVideoUrl: SafeResourceUrl;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private sanitizer: DomSanitizer) {
+    this.currentLesson = this.modules[this.currentModuleIndex].lessons[this.currentLessonIndex];
+    this.safeVideoUrl = this.sanitizeUrl(this.currentLesson.videoLink);
   }
+
+  ngOnInit(): void {}
 
   setLesson(lesson: Lesson): void {
     this.currentLesson = lesson;
+    this.safeVideoUrl = this.sanitizeUrl(lesson.videoLink);
+  }
+
+  sanitizeUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  isFirstLesson(): boolean {
+    return this.currentLessonIndex === 0 && this.currentModuleIndex === 0;
+  }
+
+  isLastLesson(): boolean {
+    return this.currentModuleIndex === this.modules.length - 1 &&
+           this.currentLessonIndex === this.modules[this.currentModuleIndex].lessons.length - 1;
+  }
+
+  prevLesson(): void {
+    if (this.currentLessonIndex > 0) {
+      this.currentLessonIndex--;
+    } else if (this.currentModuleIndex > 0) {
+      this.currentModuleIndex--;
+      this.currentLessonIndex = this.modules[this.currentModuleIndex].lessons.length - 1;
+    }
+    this.setLesson(this.modules[this.currentModuleIndex].lessons[this.currentLessonIndex]);
+  }
+
+  nextLesson(): void {
+    if (this.currentLessonIndex < this.modules[this.currentModuleIndex].lessons.length - 1) {
+      this.currentLessonIndex++;
+    } else if (this.currentModuleIndex < this.modules.length - 1) {
+      this.currentModuleIndex++;
+      this.currentLessonIndex = 0;
+    }
+    this.setLesson(this.modules[this.currentModuleIndex].lessons[this.currentLessonIndex]);
+  }
+
+  downloadResource(): void {
+    const link = document.createElement('a');
+    link.href = this.currentLesson.resourceLink;
+    link.download = this.currentLesson.resourceLink.split('/').pop()!;
+    link.click();
   }
 }
 
 interface Lesson {
   title: string;
   description: string[];
+  videoLink: string;
+  resourceLink: string;
 }
 
 interface Module {
